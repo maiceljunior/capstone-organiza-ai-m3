e79934009017e3ea33a433ed52e51c2c89ab6716
Maice Junior <junioraraujc@gmail.com>
2022-05-10 13:02:34 -0300

    add: Folder Structure

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import GlobalStyle from "./styles/global";
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+    </>
+  );
+}
+
+export default App;
