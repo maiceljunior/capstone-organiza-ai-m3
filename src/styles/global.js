7ca5eb3bfbb8516fa2ded8555b23c2124feb42fb
Danilo <danilo@gmail.com>
2022-05-11 11:25:22 -0300

    add: GlobalStyles, Pages Structure

--- src/styles/global.js
+++ src/styles/global.js
@@ -21,4 +21,21 @@ export default createGlobalStyle`
     li {
         list-style: none;
     }
+
+    :root{
+        --black: #000000,
+        --grey-dark: #212529,
+        --grey: #868E96,
+        --grey-light:#E9ECEF,
+        --white-dark: #F8F9FA,
+        --white: #FFFFFF,
+        --blue-dark: #03071E,
+        --brown: #370617,
+        --red-dark: #6A040F,
+        --red: #9D0208,
+        --red-light: #DC2F02,
+        --orange: #E85D04,
+        --gold: #F48C06,
+        --yellow: #FAA307,
+    }
 `;
