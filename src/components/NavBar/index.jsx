9a327540b537775fe795dd6749309b8abd35d6e5
Luiz Assano <luizaugusto1974@hotmail.com>
2022-05-12 13:33:27 -0300

    nav-bar arrumada mobile

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,24 +1,47 @@
-import { useHistory } from "react-router-dom";
-import Button from "../Button";
-import { Container } from "./style";
-import { useLocation } from "react-router-dom";
+import React, { useState } from 'react';
+import * as FaIcons from 'react-icons/fa';
+import * as AiIcons from 'react-icons/ai';
+import * as IoIcons from 'react-icons/io';
+import { Link } from 'react-router-dom';
+import { SidebarData } from './sidebarData';
+import './styles.css';
+import { IconContext } from 'react-icons';
 
-const NavBar = () => {
-  const history = useHistory();
-  const location = useLocation();
-  const currentLocation = location.pathname;
+function Navbar() {
+  const [sidebar, setSidebar] = useState(false);
+
+  const showSidebar = () => setSidebar(!sidebar);
 
   return (
-    <Container>
-      {currentLocation === "/register" && (
-        <Button
-          onClick={() => history.push("/login")}
-        >
-          Login
-        </Button>
-      )}
-    </Container>
+    <>
+      <IconContext.Provider value={{ color: '#fff' }}>
+        <div className='navbar'>
+          <Link to='#' className='menu-bars'>
+            <IoIcons.IoIosArrowForward onClick={showSidebar} />
+          </Link>
+        </div>
+        <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
+          <ul className='nav-menu-items' onClick={showSidebar}>
+            <li className='navbar-toggle'>
+              <Link to='#' className='menu-bars'>
+                <AiIcons.AiOutlineClose />
+              </Link>
+            </li>
+            {SidebarData.map((item, index) => {
+              return (
+                <li key={index} className={item.cName}>
+                  <Link to={item.path}>
+                    {item.icon}
+                    <span>{item.title}</span>
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+      </IconContext.Provider>
+    </>
   );
-};
+}
 
-export default NavBar;
+export default Navbar;
