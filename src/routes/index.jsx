4e8c456db315860eaad99d841176fac3d38eb904
Danilo <danilo@gmail.com>
2022-05-12 13:18:24 -0300

    add: Redirect token verification

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,12 +1,15 @@
-import { Switch, Route } from "react-router-dom";
+import { Switch } from "react-router-dom";
+import Dashboard from "../pages/Dashboard";
 import HomePage from "../pages/Home";
 import Login from "../pages/Login";
+import Route from "./route";
 
 function Routes() {
   return (
     <Switch>
       <Route exact path="/" component={HomePage} />
       <Route path="/login" component={Login} />
+      <Route path="/dashboard" component={Dashboard} isPrivate />
     </Switch>
   );
 }
