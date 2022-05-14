763b063d0475c6c705f48e9281c3b912b650d2aa
Maice Junior <junioraraujc@gmail.com>
2022-05-14 03:55:07 -0300

    add: Dashboard desktop and functionalities

--- src/pages/Dashboard/style.js
+++ src/pages/Dashboard/style.js
@@ -1,5 +1,47 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 export const Main = styled.main`
-display: flex;
+  display: flex;
+
+  .nav-menu {
+    height: 50vh;
+    margin-left: 2vw;
+    border-top-right-radius: 60px;
+    border-bottom-right-radius: 60px;
+    border-bottom-left-radius: 0;
+    border-top-left-radius: 0;
+    background-color: var(--orange);
+    z-index: 3000;
+  }
+
+  .navbar-toggle {
+    background-color: var(--orange);
+  }
+
+  .navbar {
+    background-color: var(--black);
+    margin-top: 30vh;
+  }
+
+  .nav-text > a:hover {
+    background-color: #6a040f;
+  }
+
+  .nav-text > a > span {
+    color: var(--white);
+    font-family: "Nunito", sans-serif;
+    font-weight: bold;
+  }
+
+  #Search {
+    width: 100%;
+    height: 45px;
+    align-items: center;
+    align-self: center;
+    margin: 0;
+  }
+
+  p {
+    display: none;
+  }
 `;
