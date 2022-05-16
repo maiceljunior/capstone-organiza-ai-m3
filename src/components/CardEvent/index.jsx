8599d8a6c7b060f33fbeab33cb32dab5da6555c0
Danilo <danilo@gmail.com>
2022-05-16 13:38:27 -0300

    update: Add person to event

--- src/components/CardEvent/index.jsx
+++ src/components/CardEvent/index.jsx
@@ -6,83 +6,77 @@ import {
   ButtonEventWrapper,
 } from "./style";
 import EventoIcon from "../../assets/imgs/EventoIcon.png";
-import FutebolIcon from "../../assets/imgs/Futebol.jpg"
-import XadrezIcon from "../../assets/imgs/Xadrez.jpg"
-import RpgIcon from "../../assets/imgs/Rpg.png"
-import TabuleiroIcon from "../../assets/imgs/Tabuleiro.png"
-import OnlineIcon from "../../assets/imgs/Online.png"
+import FutebolIcon from "../../assets/imgs/Futebol.jpg";
+import XadrezIcon from "../../assets/imgs/Xadrez.jpg";
+import RpgIcon from "../../assets/imgs/Rpg.png";
+import TabuleiroIcon from "../../assets/imgs/Tabuleiro.png";
+import OnlineIcon from "../../assets/imgs/Online.png";
 import { Api } from "../../services/api";
 import { useUser } from "../../providers/user";
 
 const CardEvent = ({ event }) => {
-  const { user } = useUser()
-  const { nameEvent, description, type, id, userId, guests, eventToken } = event;
+  const { user } = useUser();
+  const { nameEvent, description, type, id, userId, guests, eventToken } =
+    event;
   const userLogged = localStorage.getItem("UserID");
 
-
   function joinEvent(usuario) {
-
     if (guests.includes(usuario)) {
-      alert("ja existe esse usuario")
+      alert("ja existe esse usuario");
     } else {
-      guests.push(usuario)
-      const newGuest = [...guests, usuario]
+      const newGuest = [...guests, usuario];
 
-      // joinEventApi(newGuest)
-
+      console.log(event);
+      joinEventApi(newGuest);
     }
-
   }
 
   function joinEventApi(data) {
-    Api.patch(`eventsPublics/${id}`, data, {
-      headers: {
-        Authorization: `Bearer ${eventToken}`,
-      },
-    })
+    Api.patch(
+      `eventsPublics/${id}`,
+      { guests: data },
+      {
+        headers: {
+          Authorization: `Bearer ${eventToken}`,
+        },
+      }
+    )
       .then((res) => console.log(res))
-      .catch((err) => console.log(err))
-
-  };
+      .catch((err) => console.log(err));
+  }
 
   return (
     <MainWrapper>
       <ImgEventWrapper>
-        {
-          type === "Futebol" ?
-            <figure>
-              <img src={FutebolIcon} alt="Img Evento" />
-            </figure>
-            :
-            type === "Xadrez" ?
-              <figure>
-                <img src={XadrezIcon} alt="Img Evento" />
-              </figure>
-              :
-              type === "RPG" ?
-                <figure>
-                  <img src={RpgIcon} alt="Img Evento" />
-                </figure>
-                :
-                type === "Tabuleiro" ?
-                  <figure>
-                    <img src={TabuleiroIcon} alt="Img Evento" />
-                  </figure>
-                  :
-                  type === "Online" ?
-                    <figure>
-                      <img src={OnlineIcon} alt="Img Evento" />
-                    </figure>
-                    :
-                    type === "Outros" ?
-                      <figure>
-                        <img className="Outros" src={EventoIcon} alt="Img Evento" />
-                      </figure>
-                      :
-                      <figure>
-                        <img className="Outros" src={EventoIcon} alt="Img Evento" />
-                      </figure>
-        }
+        {type === "Futebol" ? (
+          <figure>
+            <img src={FutebolIcon} alt="Img Evento" />
+          </figure>
+        ) : type === "Xadrez" ? (
+          <figure>
+            <img src={XadrezIcon} alt="Img Evento" />
+          </figure>
+        ) : type === "RPG" ? (
+          <figure>
+            <img src={RpgIcon} alt="Img Evento" />
+          </figure>
+        ) : type === "Tabuleiro" ? (
+          <figure>
+            <img src={TabuleiroIcon} alt="Img Evento" />
+          </figure>
+        ) : type === "Online" ? (
+          <figure>
+            <img src={OnlineIcon} alt="Img Evento" />
+          </figure>
+        ) : type === "Outros" ? (
+          <figure>
+            <img className="Outros" src={EventoIcon} alt="Img Evento" />
+          </figure>
+        ) : (
+          <figure>
+            <img className="Outros" src={EventoIcon} alt="Img Evento" />
+          </figure>
+        )}
       </ImgEventWrapper>
       <InfoEventWrapper>
         <h2>{nameEvent}</h2>
@@ -90,11 +84,15 @@ const CardEvent = ({ event }) => {
         <p>{description}</p>
       </InfoEventWrapper>
       <ButtonEventWrapper>
-        {parseInt(userLogged) === userId ?
-          <ButtonComponent onClick={() => console.log("editar")} redSchema>Editar</ButtonComponent>
-          :
-          <ButtonComponent onClick={() => joinEvent(user)}>Participar</ButtonComponent>
-        }
+        {parseInt(userLogged) === userId ? (
+          <ButtonComponent onClick={() => console.log("editar")} redSchema>
+            Editar
+          </ButtonComponent>
+        ) : (
+          <ButtonComponent onClick={() => joinEvent(user)}>
+            Participar
+          </ButtonComponent>
+        )}
       </ButtonEventWrapper>
     </MainWrapper>
   );
